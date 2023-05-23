215e7fc5b8f41923042aaf74fdfbed3e1246131f
윤민영 <yoonmy7@naver.com>
2023-05-23 18:06:37 +0900

    Day5 commit

---
+++ Day5_Front_dev/final-pjt-front/src/store/index.js
@@ -0,0 +1,145 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import axios from 'axios'
+import createPersistedState from 'vuex-persistedstate'
+import router from '../router'
+
+const API_URL = 'http://127.0.0.1:8000'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  plugins: [
+    createPersistedState(),
+  ],
+  state: {
+    // 로그인
+    token: null,
+
+    // username
+    userId : null,
+
+    // 게시글
+    posts: [],
+
+    // movies
+    movies: [],
+  },
+  getters: {
+    isLogin(state){
+      return state.token ? true : false
+    }
+  },
+  mutations: {
+    // 토큰 방식 로그인 
+    SAVE_TOKEN(state, token) {
+      state.token = token
+      router.push({name : 'Homeview'}) // store/index.js $router 접근 불가 -> import를 해야함
+    },
+    
+    SAVE_OTHERS(state, { token, userId }) {
+      state.token = token
+      state.userId = userId
+      router.push({name : 'Homeview'}) // store/index.js $router 접근 불가 -> import를 해야함
+    },
+
+
+    // community post 
+    GET_POSTS(state, posts) {
+      state.posts = posts
+    },
+
+    GET_MOVIES(state, movies) {
+      state.movies = movies
+      console.log(movies)
+    },
+  },
+  actions: {
+    // 회원 가입 구현
+    signUp(context, payload) {
+      const username = payload.username
+      const password1 = payload.password1
+      const password2 = payload.password2
+
+      axios({
+        method: 'post',
+        url: `${API_URL}/accounts/signup/`,
+        data: {
+          username, password1, password2
+        }
+      })
+        .then((res) => {
+          // console.log(res)
+          // context.commit('SIGN_UP', res.data.key)
+          context.commit('SAVE_TOKEN', res.data.key)
+        })
+        .catch((err) => {
+        console.log(err)
+      })
+    },
+
+    // 로그인 구현
+    login(context, payload){
+      const username = payload.username
+      const password = payload.password
+
+      axios({
+        method: 'post',
+        url: `${API_URL}/accounts/login/`,
+        data: {
+          username, 
+          password
+        }
+      })
+      .then((res)=> {
+        const token = res.data.key
+        console.log(token)
+        axios({
+          method: 'get',
+          url: `${API_URL}/user/userid/`,  // URL to retrieve user ID based on token (customize as needed)
+          headers: {
+            Authorization: `Token ${token}`
+          }
+        })
+          .then((response) => {
+            const userId = response.data.userId
+            context.commit("SAVE_OTHERS", { token, userId })
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+      })
+      // .then((res)=> {
+      //   context.commit("SAVE_TOKEN", res.data.key)
+      // })
+      .catch((err)=> console.log(err))
+    },
+    
+    // posts 구현
+    getPosts(context){
+      axios({
+        method: 'get',
+        url: `${API_URL}/community/`,
+        })
+        .then((res)=>
+          context.commit('GET_POSTS', res.data)
+        )
+        .catch((err)=> console.log(err))
+    },
+
+    // movies 구현
+    getMovies(context) {
+      axios({
+        method: 'get',
+        url: `${API_URL}/movies/`,
+      })
+      .then((res)=>
+      context.commit('GET_MOVIES', res.data))
+      .catch((err)=> console.log(err))
+    }
+
+  },
+  modules: {
+  }
+})
---
+++ Day5_Front_dev/final_pjt_back/community/views.py
@@ -0,0 +1,120 @@
+from django.shortcuts import render
+from .models import Post, Comment, Reply
+from .serializer import PostListSerializer, PostDetailSerializer, CommentSerializer, ReplySerializer
+from rest_framework.response import Response
+
+# 인증 기능 추가
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+
+#http request
+from rest_framework import status  # 상태 알려주는 status
+from django.shortcuts import get_list_or_404, get_object_or_404
+from rest_framework.decorators import api_view
+
+
+# 리스트 불러오기, post 생성하기
+@api_view(['GET','POST'])
+# @permission_classes([IsAuthenticated])
+def post_list(request):
+    if request.method == 'GET':
+        post_list = Post.objects.all()
+        serializer = PostListSerializer(post_list, many =True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = PostDetailSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+# Post Detail
+@api_view(['GET','DELETE', 'PUT'])
+# @permission_classes([IsAuthenticated])
+def post_detail(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    if request.method == 'GET':
+        serializer = PostDetailSerializer(post)
+        return Response(serializer.data)
+    
+    elif request.method =='DELETE':
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == 'PUT':
+        serializer = PostDetailSerializer(post, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        
+
+# Comment
+@api_view(['GET'])
+def comment_list(request):
+    if request.method == 'GET':
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+    
+
+@api_view(['GET','DELETE', 'PUT'])
+def comment_detail(request, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    if request.method == 'GET':
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+    
+    elif request.method == 'DELETE':
+        comment.delete()
+
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+    
+
+@api_view(['POST'])
+def comment_create(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(post=post)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+
+@api_view(['GET'])
+def reply_list(request):
+    if request.method == 'GET':
+        reply = Reply.objects.all()
+        serializer = CommentSerializer(reply, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET','DELETE', 'PUT'])
+def reply_detail(request, reply_pk):
+    reply = get_object_or_404(Reply, pk=reply_pk)
+    if request.method == 'GET':
+        serializer = CommentSerializer(reply)
+        return Response(serializer.data)
+    
+    elif request.method == 'DELETE':
+        reply.delete()
+
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(reply, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+
+@api_view(['POST'])
+def reply_create(request, post_pk, comment_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    serializer = ReplySerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(post=post, comment=comment)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
