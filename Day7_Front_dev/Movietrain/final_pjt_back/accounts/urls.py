1f83a33808939d6a3fd94cb7d8173cd3efee8ff6
윤민영 <yoonmy7@naver.com>
2023-05-26 15:45:42 +0900

    Final

--- Day7_Front_dev/Movietrain/final_pjt_back/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('userid/', views.user_profile),
-    path('profile/<str:username>/', views.get_user_profile),
-    path('profile/<str:username>/<str:yourname>/', views.follow),
-]
